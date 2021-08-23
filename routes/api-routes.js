5ca733b950c0647cc8c20cc1d2f8c6ef9ba015ce
angelz510 <angelzrod4@gmail.com>
2021-08-23 15:20:22 -0700

    cloned and downloaded deps

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 require("dotenv").config();
 
 router.get("/api", (req, res) => {
-  console.log(process.env.API_KEY);
+  console.log(process.env.GLIZZY_KEY);
   res.json({ msg: "success" });
 });
 
