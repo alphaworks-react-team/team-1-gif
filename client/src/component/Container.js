378952ab13ec6796376619d4dbbfdcb3d7d8317d
Akiko Green <agreen@alphaworks.tech>
2021-08-24 13:39:05 -0700

    GRADIENT COLORS

--- client/src/component/Container.js
+++ client/src/component/Container.js
@@ -2,14 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = (props) => {
+  
   const Container = styled.div`
     border: 2px solid red;
     display: flex;
-    align-items: center;
-    justify-content: space-around;
     flex-flow: row wrap;
-    /* width: 60vw;
-    height: 100%; */
+    margin: 0 2rem 0 2rem;
   `;
 
   return <Container>{props.children}</Container>;
