51416afa349da67e3e6cfbede57b0e3467761f6d
angelz510 <angelzrod4@gmail.com>
2021-08-26 13:28:13 -0700

    fixed styled component err

--- client/src/component/Container.js
+++ client/src/component/Container.js
@@ -1,18 +1,11 @@
-import React from "react";
 import styled from "styled-components";
 
-const Container = (props) => {
-  
-  const Container = styled.div`
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: center;
-    margin: 0 8rem 0 8rem;
-
-    margin-bottom: 5rem;
-  `;
-
-  return <Container>{props.children}</Container>;
-};
+const Container = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  margin: 0 8rem 0 8rem;
+  margin-bottom: 5rem;
+`;
 
 export default Container;
