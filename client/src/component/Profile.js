869442d81f024a7d711000af11bce132a3335d6f
Akiko Green <agreen@alphaworks.tech>
2021-08-26 13:20:17 -0700

    more changes

--- client/src/component/Profile.js
+++ client/src/component/Profile.js
@@ -8,18 +8,18 @@ const Profile = () => {
     flex-direction: column;
     background-color:lightblue;
     width: 100vw;
-    height: 500%;
+    height: 300%;
   `;
 
   const ProfilePic = styled.div`
     display: flex;
-    width: 100px;
-    height: 100px;
+    width: 150px;
+    height: 150px;
     border-radius: 50%;
     background-image: url("https://www.kindpng.com/picc/m/104-1046489_sadcat-meme-memes-sad-cat-crying-cat-meme.png");
     background-position: center;
     background-size: cover;
-
+  margin-bottom: 30px;
   `;
 
   const FavoritesContainer = styled.div`
